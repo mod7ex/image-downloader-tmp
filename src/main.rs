5adaf3457985fb6f233fced1844f74d5b29835e5
Mourad RL CADI <mouradface1998@gmail.com>
2023-01-05 23:47:48 +0100

    save

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use error_chain::error_chain;
+use std::io::copy;
+use std::fs::File;
+use tempfile::Builder;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let tmp_dir = Builder::new().prefix("example-").tempdir()?;
+
+    let target = "https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712__340.jpg";
+
+    let response = reqwest::get(target).await?;
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() {None} else {Some(name)})
+            .unwrap_or("tmp.bin");
+
+            println!("file to download: {}", fname);
+
+            let fname = tmp_dir.path().join(fname);
+
+            println!("file will be located under: {:?}", fname);
+
+            File::create(fname)?
+        };
+
+    let content =  response.text().await?;
+    copy(&mut content.as_bytes(), &mut dest)?; 
+    
+    Ok(())
+    
+}
